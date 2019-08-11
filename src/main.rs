033625851e625d70d5d211fd2c91e9db9c6d8448
K <kamiyaowl@gmail.com>
2019-08-11 13:46:14 +0900

    semihostingでHello Worldできた

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// extern crate panic_abort; // requires nightly
+// extern crate panic_itm; // logs messages over ITM; requires ITM support
+// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+
+use cortex_m_semihosting::hprintln;
+use stm32f7::stm32f7x9;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = stm32f7x9::Peripherals::take().unwrap();
+
+    loop {
+        hprintln!("Hello stm32f7!").unwrap();
+    }
+}
